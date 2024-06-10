3f463a356e942561ee52b4a36e2a35d4379d79dc
naburnm8 <alint@bk.ru>
2024-06-10 19:50:42 +0300

    MainActivity work in progress

--- app/src/main/java/ru/naburnm8/bmstu/android/datamanagementnirapp/MainMenuActivity.java
+++ app/src/main/java/ru/naburnm8/bmstu/android/datamanagementnirapp/MainMenuActivity.java
@@ -7,5 +7,6 @@ public class MainMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_activity);
     }
 }
